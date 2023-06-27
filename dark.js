7341c1cf8d20c95c5ee8eb522575122da54b66d6
pusheando las notas del curso js <lautarodisalvo5@gmail.com>
2023-06-27 16:50:36 -0300

    cambios

--- dark.js
+++ dark.js
@@ -3,8 +3,8 @@ import dark from "/index.js";
 
 
 let d = document
+dark('.button-theme', 'body-dark')
 
 d.addEventListener('DOMContentLoaded', e => {
-    dark('.button-theme', 'body-dark')
     
 })
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let d = document
-
+let Ls = localStorage
 
 export default function dark(btn, classDark) {
     let $btnLight = d.querySelector(btn);
@@ -7,33 +7,47 @@ export default function dark(btn, classDark) {
     let $darkAtribute = d.querySelectorAll('[data-dark]')
     
     // let $btnLight = d.querySelector('.button-theme');
-    console.log($darkAtribute)
+    // console.log($darkAtribute)
 
 
     let moon = '🌜',
         sun ='☀️'
 
+    
+    // FUNCIONES PARA LLAMAR
+let light = () => {
+$darkAtribute.forEach(element => {
+  element.classList.add(classDark)
+    $btnLight.textContent = sun
+    Ls.setItem('theme','dark')
+});
+}
+
+let dark = () => {
+    $darkAtribute.forEach(element => {
+        element.classList.remove(classDark);
+        $btnLight.textContent = moon
+        Ls.setItem('theme','light')
+    })
+}
+
+    
+    
         d.addEventListener('click', e => {
             if (e.target.matches(btn)) {
                 // e.preventDefault()
-            console.log($btnLight.textContent)
+            // console.log($btnLight.textContent)
 
             // $svgBTNdark.classList.remove('none')
             if ($btnLight.textContent === moon) {
-                
-                $darkAtribute.forEach(element => {
-                    element.classList.add(classDark)
-                    $btnLight.textContent = sun
-                });
+                light()
                 
                 
-                console.log($btnLight.textContent)
+                
+                // console.log($btnLight.textContent)
                 // si está el sol de contenido
             } else {
-                $darkAtribute.forEach(element => {
-                    element.classList.remove(classDark)
-                    $btnLight.textContent = moon
-                })
+                dark()
                 
             }
             }
@@ -41,6 +55,20 @@ export default function dark(btn, classDark) {
         // if 
         // $svgBTNdark.classList.remove('none')
 
+        })
+    
+    
+    d.addEventListener('DOMContentLoaded', e => {
+        // console.log(Ls.getItem('theme'))
+        if (Ls.getItem('theme') === '') {
+           Ls.setItem('theme', 'light')
+        } 
+        if (Ls.getItem("theme") === "light") {
+            dark();
+        }
+        if (Ls.getItem('theme') === 'dark') {
+            light();
+        }
     })
 
 }
